9c2ad91230d72fe6d661450cc78300ea223ae2bc
Vidhu Kant Sharma <bokuwakanojogahoshii@yahoo.com>
2020-10-19 11:05:36 +0530

    make it better

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+//	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+	//	log.Fatal("$PORT must be set")
+		port = "8080"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.html")
+	router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	router.Run(":" + port)
+}
